5b6cf6bc57eb84f1f046e19762abec2f093a55a2
AmitSheth9 <amit.sheth9@gmail.com>
2021-12-15 13:57:01 -0800

    test passing after changing babelrc file as suggestd in slack

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from "./Home";
 import { useGuest } from "../../context/GuestContext";
